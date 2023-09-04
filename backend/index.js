3a191d679dad3dcfb83bf37510fdf2e128378bb8
Cassiele Torino <ketlin.cassi121@gmail.com>
2023-09-04 11:21:34 -0300

    criacao do banco

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const app = express();
+app.use(express.json());
 
 app.listen(5587,()=> console.log("Deu certo!"));
 
-app.get('/',(req,res)=>{res.send('Aí sim!!')});
-app.get('/cachorro',(req,res)=>{res.send('Auau')});
-app.get('/fim', (req,res) =>{res.end()});
-const dados = ['Cassi'];
-app.get('/j', (req,res)=>{res.json({dados})});
+// app.get('/',(req,res)=>{res.send('Aí sim!!')});
+// app.get('/cachorro',(req,res)=>{res.send('Auau')});
+// app.get('/fim', (req,res) =>{res.end()});
+// const dados = ['Cassi'];
+// app.get('/j', (req,res)=>{res.json({dados})});
+
+const mysql = require('mysql2/promise');
+const conection = mysql.createPool({
+  host: 'localhost',
+  port: 3306,
+  database: 'testepessoa',
+  user: 'root',
+  password: ''
+});
+
+const getAllPessoas = async () =>{
+  const [query] = await conection.execute('select * from pessoa')
+  return query
+}
+
+app.get('/pessoa', async (req, res)=>{
+  const resultado = await getAllPessoas()
+  return res.status(200).json(resultado)
+});
