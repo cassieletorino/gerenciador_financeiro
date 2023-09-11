9311c4b9167414e09a05cfff038af0e97c5c60f9
Cassiele Torino <ketlin.cassi121@gmail.com>
2023-09-11 11:26:01 -0300

    metodo get e post

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const res = require('express/lib/response');
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.listen(5587,()=> console.log("Deu certo!"));
 
@@ -30,3 +31,23 @@ app.get('/pessoa', async (req, res)=>{
   const resultado = await getAllPessoas()
   return res.status(200).json(resultado)
 });
+
+app.get('/pessoa/:id', async (req,res)=>{
+  const {id} = req.params;
+  const [query] = await conection.execute('select * from pessoa where id = ?', [id]);
+  if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
+  return res.status(200).json(query)
+});
+
+app.get('/pessoa/:nome', async (req,res)=>{
+  const {nome} = req.params;
+  const [query] = await conection.execute('select * from pessoa where nome like %?%', [nome]);
+  if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
+  return res.status(200).json(query)
+})
+
+app.post('/pessoa', async (req,res) =>{
+  const {nome, email} = req.body;
+  const [query] = await conection.execute('insert into pessoa (nome, email) values (?, ?)', [nome, email]);
+  return res.json(query);
+})
