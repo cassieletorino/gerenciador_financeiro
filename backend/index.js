454370af39aa1f731cfe0ff3da055c0c0333dd12
Cassiele Torino <ketlin.cassi121@gmail.com>
2023-09-18 11:12:20 -0300

    CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const res = require('express/lib/response');
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
 app.listen(5587,()=> console.log("Deu certo!"));
 
 // app.get('/',(req,res)=>{res.send('Aí sim!!')});
@@ -39,15 +37,36 @@ app.get('/pessoa/:id', async (req,res)=>{
   return res.status(200).json(query)
 });
 
-app.get('/pessoa/:nome', async (req,res)=>{
+app.get('/pessoa/buscarnome/:nome', async (req,res)=>{
   const {nome} = req.params;
-  const [query] = await conection.execute('select * from pessoa where nome like %?%', [nome]);
+  const [query] = await conection.execute('select * from pessoa where nome like ?', ['%'+nome+'%']);
   if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
   return res.status(200).json(query)
-})
+});
+
+app.get('/pessoa/buscaremail/:email', async (req,res)=>{
+  const {email} = req.params;
+  const [query] = await conection.execute('select * from pessoa where email like ?', ['%'+email+'%']);
+  if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
+  return res.status(200).json(query)
+});
 
 app.post('/pessoa', async (req,res) =>{
   const {nome, email} = req.body;
   const [query] = await conection.execute('insert into pessoa (nome, email) values (?, ?)', [nome, email]);
+  return res.json(query);
+})
+
+app.put('/pessoa/:id', async (req,res) =>{
+  const {id} = req.params;
+  const {nome, email} = req.body;
+
+  const [query] = await conection.execute('update pessoa set nome = ?, email = ? where id = ?', [nome, email, id]);
+  return res.json(query);
+})
+
+app.delete('/pessoa/:id', async (req,res) =>{
+  const {id} = req.params;
+  const [query] = await conection.execute('delete * from pessoa where id = ?', [id]);
   return res.json(query);
 })
