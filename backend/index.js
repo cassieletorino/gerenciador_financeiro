f7ee6010730c1aabe9e4fabaafafa00fe25c856c
cassieletorino <ketlin.cassi121@gmail.com>
2023-09-25 11:24:48 -0300

    inicio db

--- backend/index.js
+++ backend/index.js
@@ -15,58 +15,24 @@ const mysql = require('mysql2/promise');
 const conection = mysql.createPool({
   host: 'localhost',
   port: 3306,
-  database: 'testepessoa',
+  database: 'mydb',
   user: 'root',
   password: ''
 });
 
-const getAllPessoas = async () =>{
-  const [query] = await conection.execute('select * from pessoa')
+const getAllUsuarios = async () =>{
+  const [query] = await conection.execute('select * from usuario')
   return query
 }
 
-app.get('/pessoa', async (req, res)=>{
-  const resultado = await getAllPessoas()
+app.get('/usuarios', async (req, res)=>{
+  const resultado = await getAllUsuarios()
   return res.status(200).json(resultado)
 });
 
-app.get('/pessoa/:id', async (req,res)=>{
+app.get('/usuario/:id', async (req,res)=>{
   const {id} = req.params;
-  const [query] = await conection.execute('select * from pessoa where id = ?', [id]);
-  if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
+  const [query] = await conection.execute('select * from usuario where id = ?', [id]);
+  if (query.length===0) return res.status(400).json({mensagem: 'nenhum usuario encontrado'});
   return res.status(200).json(query)
 });
-
-app.get('/pessoa/buscarnome/:nome', async (req,res)=>{
-  const {nome} = req.params;
-  const [query] = await conection.execute('select * from pessoa where nome like ?', ['%'+nome+'%']);
-  if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
-  return res.status(200).json(query)
-});
-
-app.get('/pessoa/buscaremail/:email', async (req,res)=>{
-  const {email} = req.params;
-  const [query] = await conection.execute('select * from pessoa where email like ?', ['%'+email+'%']);
-  if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
-  return res.status(200).json(query)
-});
-
-app.post('/pessoa', async (req,res) =>{
-  const {nome, email} = req.body;
-  const [query] = await conection.execute('insert into pessoa (nome, email) values (?, ?)', [nome, email]);
-  return res.json(query);
-})
-
-app.put('/pessoa/:id', async (req,res) =>{
-  const {id} = req.params;
-  const {nome, email} = req.body;
-
-  const [query] = await conection.execute('update pessoa set nome = ?, email = ? where id = ?', [nome, email, id]);
-  return res.json(query);
-})
-
-app.delete('/pessoa/:id', async (req,res) =>{
-  const {id} = req.params;
-  const [query] = await conection.execute('delete * from pessoa where id = ?', [id]);
-  return res.json(query);
-})
